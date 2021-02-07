6a3b952aa002221bae775c166b532e34e1cfc51d
sorahiro08 <kattyamu0830@gmail.com>
2021-02-07 15:55:23 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 
 function App() {
+  const greeting = "Hi! Tom!";
+  const dom = <h1 className="foo">{greeting}</h1>;
   return (
-    <h1>Hello, world!!</h1>
+    <>
+      {dom}
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={ () => {console.log("I am Changed!")} } />
+    </>
   );
 }
 
